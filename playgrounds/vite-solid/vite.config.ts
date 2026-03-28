55ab6015fc24f8d1b6cb0a8e0ef0b9a4da28ad4a
Badi Ifaoui <badi.ifaoui@zixdev.com>
2026-03-28 21:29:51 +0100

    feat: enhance Vite configuration with package aliases for playgrounds
    - Added path resolution and aliasing for `@growae/reactive` packages in Vite configuration files for React, Solid, and Vue playgrounds.
    - This update improves module resolution and streamlines imports across different frameworks, facilitating better development experience.

--- playgrounds/vite-solid/vite.config.ts
+++ playgrounds/vite-solid/vite.config.ts
@@ -1,6 +1,35 @@
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const pkgs = resolve(__dirname, '../../packages')
+
 export default defineConfig({
   plugins: [solid()],
+  resolve: {
+    alias: {
+      '@growae/reactive/actions': resolve(pkgs, 'core/src/exports/actions.ts'),
+      '@growae/reactive/query': resolve(pkgs, 'core/src/exports/query.ts'),
+      '@growae/reactive/networks': resolve(
+        pkgs,
+        'core/src/exports/networks.ts',
+      ),
+      '@growae/reactive/internal': resolve(
+        pkgs,
+        'core/src/exports/internal.ts',
+      ),
+      '@growae/reactive': resolve(pkgs, 'core/src/exports/index.ts'),
+      '@growae/reactive-solid/query': resolve(
+        pkgs,
+        'solid/src/exports/query.ts',
+      ),
+      '@growae/reactive-solid': resolve(pkgs, 'solid/src/exports/index.ts'),
+      '@growae/reactive-connectors': resolve(
+        pkgs,
+        'connectors/src/exports/index.ts',
+      ),
+    },
+  },
 })
